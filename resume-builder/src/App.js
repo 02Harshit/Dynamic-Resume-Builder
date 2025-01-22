ea5a14e552f32cdfe8c413a936b298c68c4213c3
02Harshit <mittalharshit2004@gmail.com>
2025-01-22 10:02:42 +0530

    About us component created and integrated
    In this commit, i have created a about us page for the website with the necessary components and animations used.

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -2,9 +2,14 @@ import logo from './assets/logo.svg';
 import './App.css';
 import Login from './components/login';
 import LandingPage from './components/landing_page_component.js';
+import AboutUs from './components/aboutUs.js';
+
 function App() {
   return (     
+    <>
     <LandingPage />
+    <AboutUs />
+    </>
   );
 }
 
--- resume-builder/src/components/aboutUs.js
+++ resume-builder/src/components/aboutUs.js
@@ -1,15 +1,24 @@
 import styles from './aboutUs.module.css';
-
+import SpotlightCard from '../assets/spotlightCard/SpotlightCard.jsx';
+import About3D from './About3D.js';
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className={styles.aboutUsSection}>
             <div className={styles.headingContainer}>
                 <span className={styles.headingText}> About Us </span>
             </div>
             <div className={styles.mainContainer}>
-                <div className={styles.cardContainer}></div>
-                <div className={styles.animationContainer}></div>
+                <div className={styles.cardContainer}>
+                <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <h2 className={styles.cardHeading}>Say Goodbye to paid resume builders!</h2><br></br>
+                <p className={styles.cardText}>Our aim is to make resume building easy with the help of our website, it offers various easy to use templates, which will help you to easily create your resume and land into your dream company!</p>
+                
+                </SpotlightCard>
+                </div>
+                <div className={styles.animationContainer}>
+                    <About3D />
+                </div>
             </div>
         </div>
     );
