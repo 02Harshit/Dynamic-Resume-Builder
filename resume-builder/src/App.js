34caba312d909fbd76bc5d9a91ed0c7c34b64748
SJ0900777 <sj0900777@gmail.com>
2025-01-24 00:15:33 +0530

    Nav_css
    Changes made in navbar css

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,20 +1,19 @@
-import logo from './assets/logo.svg';
-import './App.css';
-import Login from './components/login';
-import LandingPage from './components/landing_page_component.js';
-import AboutUs from './components/aboutUs.js';
-import DeveloperPage from './components/developerPage.js';
+import logo from "./assets/logo.svg";
+import "./App.css";
+import Login from "./components/login";
+import LandingPage from "./components/landing_page_component.js";
+import AboutUs from "./components/aboutUs.js";
+import DeveloperPage from "./components/developerPage.js";
 
 function App() {
   return (
     <>
-    <LandingPage />
-    <AboutUs />
-    <DeveloperPage />
-    <Login />
+      <LandingPage />
+      <AboutUs />
+      <DeveloperPage />
+      <Login />
     </>
   );
 }
-
 
 export default App;
