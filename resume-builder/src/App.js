7d60492e86e0f96b072053def587070fe5224bfd
SJ0900777 <sj0900777@gmail.com>
2025-01-23 21:40:08 +0530

    Update App.js

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,17 +1,18 @@
-import logo from './assets/logo.svg';
-import './App.css';
-import Login from './components/login';
-import LandingPage from './components/landing_page_component.js';
-import AboutUs from './components/aboutUs.js';
-import DeveloperPage from './components/developerPage.js';
+import logo from "./assets/logo.svg";
+import "./App.css";
+import Login from "./components/login";
+import LandingPage from "./components/landing_page_component.js";
+import AboutUs from "./components/aboutUs.js";
+import DeveloperPage from "./components/developerPage.js";
+import Login_render from "./components/Loginn/Login_render.js";
 
 function App() {
-  return (     
+  return (
     <>
-    <LandingPage />
-    <AboutUs />
-    <DeveloperPage />
-    <Login />
+      <LandingPage />
+      <AboutUs />
+      <DeveloperPage />
+      <Login_render />
     </>
   );
 }
