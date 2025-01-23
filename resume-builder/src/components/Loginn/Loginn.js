9ba676ccb72fecfa0f639fde2dacb6055eddeff4
SJ0900777 <sj0900777@gmail.com>
2025-01-23 21:25:11 +0530

    Changes in Loginn
    Check it once

--- resume-builder/src/components/Loginn/Loginn.js
+++ resume-builder/src/components/Loginn/Loginn.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import styles from "./Login1.module.css";
+import "./style.css";
 
-const Login1 = () => {
+const Loginn = () => {
   const [isActive, setIsActive] = useState(false);
 
   const handleRegisterClick = () => {
@@ -13,25 +13,21 @@ const Login1 = () => {
   };
 
   return (
-    <div
-      className={`${styles.container} ${isActive ? styles.active : ""}`}
-      id="container"
-    >
-      {/* Sign Up Form */}
-      <div className={`${styles.formContainer} ${styles.signUp}`}>
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
         <form>
           <h1>Create Account</h1>
-          <div className={styles.socialIcons}>
-            <a href="#" className={styles.icon}>
+          <div className="social-icons">
+            <a href="#" className="icon">
               <i className="fa-brands fa-google-plus-g"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-facebook-f"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-github"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-linkedin-in"></i>
             </a>
           </div>
@@ -42,50 +38,46 @@ const Login1 = () => {
           <button>Sign Up</button>
         </form>
       </div>
-
-      {/* Sign In Form */}
-      <div className={`${styles.formContainer} ${styles.signIn}`}>
+      <div className="form-container sign-in">
         <form>
           <h1>Sign In</h1>
-          <div className={styles.socialIcons}>
-            <a href="#" className={styles.icon}>
+          <div className="social-icons">
+            <a href="#" className="icon">
               <i className="fa-brands fa-google-plus-g"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-facebook-f"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-github"></i>
             </a>
-            <a href="#" className={styles.icon}>
+            <a href="#" className="icon">
               <i className="fa-brands fa-linkedin-in"></i>
             </a>
           </div>
-          <span>or use your email password</span>
+          <span>or use your email and password</span>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <a href="#">Forgot Your Password?</a>
+          <a href="#">Forget Your Password?</a>
           <button>Sign In</button>
         </form>
       </div>
-
-      {/* Toggle Panels */}
-      <div className={styles.toggleContainer}>
-        <div className={styles.toggle}>
-          <div className={`${styles.togglePanel} ${styles.toggleLeft}`}>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
             <h1>Welcome Back!</h1>
             <p>Enter your personal details to use all of the site features</p>
-            <button className={styles.hidden} onClick={handleLoginClick}>
+            <button className="hidden" onClick={handleLoginClick}>
               Sign In
             </button>
           </div>
-          <div className={`${styles.togglePanel} ${styles.toggleRight}`}>
+          <div className="toggle-panel toggle-right">
             <h1>Hello, Friend!</h1>
             <p>
               Register with your personal details to use all of the site
               features
             </p>
-            <button className={styles.hidden} onClick={handleRegisterClick}>
+            <button className="hidden" onClick={handleRegisterClick}>
               Sign Up
             </button>
           </div>
@@ -95,4 +87,4 @@ const Login1 = () => {
   );
 };
 
-export default Login1;
+export default Loginn;
