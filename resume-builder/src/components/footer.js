719fb494f4bb8c38d7c8a898bab8b801a2312df7
SJ0900777 <sj0900777@gmail.com>
2025-01-24 21:59:44 +0530

    footer section added
    footer section added

---
+++ resume-builder/src/components/footer.js
@@ -0,0 +1,12 @@
+// footer.js
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright © Resume-Builder . All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
