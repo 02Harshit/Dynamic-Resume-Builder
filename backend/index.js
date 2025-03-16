3e41700ef0a31b12b702154e22fb920144553291
02Harshit <mittalharshit2004@gmail.com>
2025-03-16 19:37:30 +0530

    backend initiation

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+//importing necessary packages/modules
+const express = require('express'); // Require function will fetch the express module and assign it to a constant variable named express.
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//Initialize express app
+const app = express();
+
+//Middleware
